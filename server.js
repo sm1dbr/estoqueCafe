5a21b08f32b669ba56c3bb8e16fe9fded58056a6
Débora Matos <smdebora@icloud.com>
2025-08-03 19:41:44 -0300

    Corrigindo erros no backend

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 
 // CONECTAR COM O SQLITE
