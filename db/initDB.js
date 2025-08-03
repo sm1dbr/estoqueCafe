3876de608bc6880d8015e667a3bc3881182b0811
Débora Matos <smdebora@icloud.com>
2025-08-03 19:22:04 -0300

    Trocando o BD para o SQLite pq o freedb é limitante demais

---
+++ db/initDB.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/estoqueCafe.bd');
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS entregas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fornecedor TEXT NOT NULL,
+        item TEXT NOT NULL,
+        quantidade INTEGER NOT NULL,
+        observacao TEXT,
+        dataHora TEXT NOT NULL
+        )`);
+});
+
+db.close();
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,20 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Criar conexão com o BD
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+// CONECTAR COM O SQLITE
+const db = new sqlite3.Database('./db/estoqueCafe.bd', (err) => {
+    if (err) {
+        console.error('Erro ao conectar com o SQLite:', err.message);
+    } else {
+        console.log('Conectado ao banco de dados SQLite!')
+    }
 });
-console.log('DB_HOST usado:', process.env.DB_HOST);
-
-
 
 // Rota pra receber os dados do formulário
 
@@ -30,25 +24,28 @@ app.post('/api/entrega', (req, res) => {
 
     console.log('Dados Recebidos:', dadosRecebidos);
 
-    const sql = 'INSERT INTO entregas (fornecedor, item, quantidade, observacao, dataHora) VALUES ?';
+    const sql = 'INSERT INTO entregas (fornecedor, item, quantidade, observacao, dataHora) VALUES (?, ?, ?, ?, ?)';
     
-    const values = dadosRecebidos.itens.map(i => [
-        dadosRecebidos.fornecedor,
-        i.item,
-        i.quantidade,
-        dadosRecebidos.observacao,
-        new Date(dadosRecebidos.dataHora)
-    ]);
+    const stmt = db.prepare(sql);
 
-    pool.query(sql, [values], (err, results) => {
-        if (err) {
-            console.error('Erro ao inserir dados:', err);
-            return res.status(500).json({ error: 'Erro ao salvar no banco' });
-        }
-
-        res.status(200).json({ message: 'Dados recebidos com sucesso!'});
-
+    dadosRecebidos.itens.forEach(i => {
+        stmt.run(
+            dadosRecebidos.fornecedor,
+            i.item,
+            i.quantidade,
+            dadosRecebidos.observacao,
+            dadosRecebidos.dataHora
+        );
     });
+
+    stmt.finalize(err => {
+        if (err) {
+            console.error('Erro ao inserir dados:', err.message);
+            return res.status(500).json({ error: 'Erro ao salvar no banco' });
+        } 
+        res.status(200).json({ message: 'Dados recebidos com sucesso!' });
+    });
+
 });
 
 // Inicia o servidor
